af2732280a7b7366b84410d291b72bf68ce066c1
Shirin Muataz <shirinmuataz@gmail.com>
2020-07-12 22:57:43 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,82 +1,54 @@
-window.onload = function () {
-    //Challenge 1
-    let sh = fetch('https://dog.ceo/api/breeds/image/random/4').then(Response => Response.json())
+console.log('%c HI', 'color: firebrick')
 
-    let image1 = document.createElement('img');
-    let image2 = document.createElement('img');
-    let image3 = document.createElement('img');
-    let image4 = document.createElement('img');
-    let body = document.querySelector('body');
-    body.style.display = 'block';
-    sh.then(resp => {
-        image1.src = resp.message[0];
-        body.appendChild(image1);
-
-        image2.src = resp.message[1];
-        body.appendChild(image2);
-
-        image3.src = resp.message[2];
-        body.appendChild(image3);
-
-        image4.src = resp.message[3];
-        body.appendChild(image4);
-    })
-
-    //Challenge 2
-
-    let ul = document.getElementById('dog-breeds');
-    const breedUrl = fetch('https://dog.ceo/api/breeds/list/all')
-        .then(rep => rep.json())
-        .then(dogs => {
-            console.log(dogs);
-            const breedMessage = dogs.message;
-            for (const breed in breedMessage) {
-                const li = document.createElement('li');
-                li.innerText = breed;
-                ul.appendChild(li);
-                li.className = 'item';
-            }
-        })
+document.addEventListener('DOMContentLoaded', function() {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const ul = document.getElementById("dog-breeds");
+const selectBreeds = document.getElementById("breed-dropdown");
 
 
-    //Challenge 3
-    ul.addEventListener("click", function (e) {
-        if (e.target && e.target.matches("li.item")) {
-            e.target.className = "selected";
-        }
-    });
+//challenge 1
+  fetch(imgUrl).then(resp => resp.json()).then(json => addDogImg(json));
 
-
-    //Challenge 4
-    const select = document.getElementById('breed-dropdown');
-    select.addEventListener('change', function () {
-        const opt = select.options[select.selectedIndex];
-        const charc = opt.value;
-
-        fetch('https://dog.ceo/api/breeds/list/all')
-            .then(rep => rep.json())
-            .then(dog => {
-                while (ul.firstChild) { /// this one to clear the list
-                    ul.removeChild(ul.firstChild);
-                }
-                console.log(ul);
-                for (const dogBreed in dog.message) {
-                    if (charc == dogBreed.charAt(0)) {
-                        console.log(dogBreed);
-                        const li = document.createElement('li');
-                        li.innerText = dogBreed;
-                        ul.appendChild(li);
-                    }
-
-                }
-            })
-        console.log(`ul is showing here ${ul}`);
-
-    })
-
-
-
-
-
+function addDogImg(json) {
+  let imgDiv = document.getElementById("dog-image-container");
+  json.message.forEach(dogUrl => {
+    imgDiv.innerHTML += `
+      <img src="${dogUrl}"><br>
+    `
+  });   
 }
 
+ //challenge 2
+
+    fetch(breedUrl).then(resp => resp.json()).then(json => addBreeds(json.message));
+
+    function addBreeds(breeds) {
+      for(const breed in breeds){
+         console.log(breed)
+         let breedli = document.createElement("li")
+         breedli.textContent = breed;
+         ul.appendChild(breedli)
+
+      }
+
+        ul.addEventListener('click', changeColor)
+
+    }
+
+ // Challenge 3
+
+function changeColor(event) {
+  event.target.style.color = "pink";
+}
+
+//cahllenge 4
+
+  selectBreeds.addEventListener('change', dog => {
+
+    let filterBreeds = breeds.filter( breed => breed[0] === dog.target.value)
+    ul.innerHTML = addBreeds(filterBreeds)
+
+  })
+
+})
